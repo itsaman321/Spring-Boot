3bd580e98106ddd822b4967cf75e98b393a3bab6
itsaman321 <aman.vji321@gmail.com>
2023-05-15 14:45:04 +0530

    user service initiated

---
+++ user-service/src/main/java/com/cognizant/userservice/models/User.java
@@ -0,0 +1,29 @@
+package com.cognizant.userservice.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer user_id ;
+
+    private String user_name ;
+
+    private String email ;
+
+    private String user_phone ;
+
+    private String password ;
+
+}
