f1a7f4c3cdb081912cabc4b23daac9bcaccf6ad5
itsaman321 <aman.vji321@gmail.com>
2023-05-21 20:28:14 +0530

    Eureka Server : Order , Inventory , Menu

--- menu-service/src/main/java/com/cognizant/menuservice/MenuServiceApplication.java
+++ menu-service/src/main/java/com/cognizant/menuservice/MenuServiceApplication.java
@@ -1,11 +1,12 @@
 package com.cognizant.menuservice;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MenuServiceApplication {
 
 	@Bean
--- order-service/src/main/java/com/cognizant/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/cognizant/orderservice/OrderServiceApplication.java
@@ -2,10 +2,12 @@ package com.cognizant.orderservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class OrderServiceApplication {
 
     @Bean
