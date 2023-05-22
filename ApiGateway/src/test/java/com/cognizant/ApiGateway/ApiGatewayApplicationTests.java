e6510ea4b1d6ea78f6f95f9fe98e4020a58835b7
itsaman321 <aman.vji321@gmail.com>
2023-05-22 16:52:28 +0530

    api-gateway integrated

---
+++ ApiGateway/src/test/java/com/cognizant/ApiGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cognizant.ApiGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
